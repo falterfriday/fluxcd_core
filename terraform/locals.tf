d82689e8cccab885c6c33a666597f8c7a3e7637d
Patrick Todd <patrick.a.todd@gmail.com>
2026-09-04 11:30:23 +0000

    ssh key pipeline fix

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,8 +1,7 @@
 locals {
-  ssh_public_keys = coalesce(
-    var.ssh_public_keys,
-    [trimspace(file(pathexpand("${var.proxmox_ssh_private_key_file}.pub")))],
-  )
+  ssh_public_keys = var.ssh_public_keys != null ? var.ssh_public_keys : [
+    trimspace(file(pathexpand("${var.proxmox_ssh_private_key_file}.pub")))
+  ]
 
   pve_nodes   = distinct([for n in var.nodes : n.pve_node])
   image_build = element(split("/", var.ubuntu_image_url), 4)
